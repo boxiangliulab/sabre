c9f92f3dd505fd79e2c97cfa4d845eb10ba2985c
Laurentiusia <caoyuan@caoyuans-MacBook-Air.local>
2023-08-31 13:06:45 +0800

    first commit

---
+++ utils/file_utils.py
@@ -0,0 +1,392 @@
+#!/usr/bin/env python
+import tempfile
+import gzip
+import numpy as np
+import pandas as pd
+import subprocess
+import tqdm
+import os
+from rich import print
+import collections
+
+class Variant:
+    def __init__(self, col_chr, col_pos, col_id, col_ref, col_alt, col_qual, genotype_string, is_phased) -> None:
+        
+        self.col_chr = col_chr
+        self.col_pos = col_pos
+        self.col_id = col_id
+        self.col_ref = col_ref
+        self.col_alt = col_alt
+        self.col_qual = col_qual
+        self.genotype_string = genotype_string
+        self.is_phased = is_phased
+
+        self.start = int(self.col_pos)-1
+        self.end = int(self.col_pos)
+
+        self.unique_id = self.create_unique_id()
+
+    def create_unique_id(self):
+        return '_'.join([self.col_chr, self.col_pos, self.col_id, self.col_ref, self.col_alt])
+    
+    def create_node(self):
+        return self.unique_id+':0', self.unique_id+':1'
+    
+    def get_geno_by_allele(self, allele):
+        if allele == self.col_ref:
+            return 0 
+        return 1
+    
+    def get_allele_by_geno(self, geno):
+        if int(geno) == 1:
+            return self.col_alt
+        return self.col_ref
+
+class Bamline:
+    def __init__(self,col_qname, col_flag, col_rname, col_pos, col_mapq, col_cigar,\
+                  col_rnext, col_pnext, col_tlen, col_seq, col_qual, alignment_score,\
+                      col_rx, col_qx, col_bx, col_bc, col_qt) -> None:
+        self.col_qname = col_qname
+        self.col_flag = col_flag
+        self.col_rname = col_rname
+        self.col_pos = col_pos
+        self.col_mapq = col_mapq
+        self.col_cigar = col_cigar
+        self.col_rnext = col_rnext
+        self.col_pnext = col_pnext
+        self.col_tlen = col_tlen
+        self.col_seq = col_seq
+        self.col_qual = col_qual
+        self.alignment_score = alignment_score
+        self.col_rx = col_rx
+        self.col_qx = col_qx
+        self.col_bx = col_bx
+        self.col_bc = col_bc
+        self.col_qt = col_qt
+
+
+class Read:
+    def __init__(self, umi_barcode:str, bamline_list:list[Bamline]) -> None:
+        self.umi_barcode = umi_barcode
+        self.bamline_list = bamline_list
+
+        self.filter_seq_by_quality()
+        self.adjust_read_seq()
+
+    def extract_read_seq(self):
+        '''
+        Extract seq and span from each bamline, then 
+        '''
+        self.read_seqs = []
+        self.read_spans = []
+        for bamline in self.bamline_list:
+            Read.
+
+    def filter_seq_by_quality(self):
+        '''
+        We first filter the read by quality
+        Any read base pair with QUAL lower than a given quality threshold will be replaced by '.'
+        '''
+        temp_seq = ''
+        for i, q in enumerate(self.qual):
+            if ord(q) - 33 < self.qual_threshold:
+                temp_seq += '.'
+            else:
+                temp_seq += self.seq[i]
+
+    def adjust_read_seq():
+        '''
+        To align variants onto reads, we need to get the real coverage of the given read to the genome, by analyzing the CIGAR string.
+        As the existance of $N$ and $D$, the sort&search algorithm may encounter difficulties if we take them into consideration.
+        Thus, the calculated real_span(the length of padded_seq_length) does contains void areas of $D$ and $N$.
+        Those offsets will be corrected by another judgement function.
+        Also, to map alleles onto reads, another preprocess need to be implemented.
+        '''
+        self.adjusted_read_seq = []
+        current_cigar_number = 0
+        seq_pointer = 0
+        for char in self.cigar:
+            if char in '0123456789':
+                current_cigar_number = current_cigar_number*10 + int(char)
+            else:
+                if char == 'S':
+                    # soft clipping
+                    # just ignore the corresponding seq by incrementing the seq pointer.
+                    seq_pointer += current_cigar_number
+                elif char == 'M' or char == '=' or char == 'X':
+                    # match
+                    # put the corresponding seq to the adjusted_read_seq, and adjust read_pointer
+                    self.adjusted_read_seq.append(self.seq[seq_pointer:seq_pointer+current_cigar_number])
+                    seq_pointer += current_cigar_number
+                elif char == 'I':
+                    # insertion
+                    # ignore the insertion by adding seq_pointer, the adjusted_read_seq will not be affected.
+                    seq_pointer += current_cigar_number
+                elif char == 'D' or char == 'N':
+                    # deletion or skipping
+                    # padd the seq with '.', it will be detected when trying to get_base_pair_by_var_pos
+                    # thus we will be notified that this read doesn't contain the variant
+                    # seq_pointer will not move cuz no corresponding subseq appears in seq
+                    self.adjusted_read_seq.append('.'*current_cigar_number)
+
+                # No operations on "H", cuz "H" is useless
+                current_cigar_number = 0
+        self.adjusted_read_seq = ''.join(self.adjusted_read_seq)
+        self.real_span = len(self.adjusted_read_seq)
+
+        # [start, end]
+        # if start <= var_pos <= end
+        # var_pos is in read
+        self.start = int(self.pos)
+        self.end = self.start + self.real_span - 1
+
+    def get_base_pair_by_var_pos(self, var_pos):
+        '''
+        Given a variant position on genome, return the corresponding base pair on the read.
+        '''
+        pos = var_pos - self.start
+        base_pair = self.adjusted_read_seq[pos]
+
+        if base_pair == '.':
+            # Means the variant actually do not appear in this read.
+            # It is covered by a deletion or skipping
+            return None
+        return base_pair
+    
+    @staticmethod
+    def split_alignment_factory(col_qname, col_flag, col_rname, col_pos, col_mapq, col_cigar, col_rnext, col_pnext, col_tlen,\
+                                 col_seq, col_qual, alignment_score):
+        '''
+        As there is the existance of $N$, need to split one read into multiple reads.
+        Only split the $col_seq$ and $col_cigar$, and recalculate the position of col_pos for the splited out read.
+        This function is recursive.
+
+        Don't ask me for why this function is working properly, I will forget it in 2 days.
+        '''
+        if col_cigar == '':
+            return []
+        current_cigar_number = 0
+        seq_pointer = 0
+        real_length = 0
+        temp_cigar = ''
+        temp_seq = ''
+
+        for i, char in enumerate(col_cigar):
+            if char in '0123456789':
+                current_cigar_number = current_cigar_number*10 + int(char)
+            else:
+                if char == 'N':
+                    # recursively split read
+                    real_length += current_cigar_number
+                    return [Read(col_qname, col_flag, col_rname, col_pos, col_mapq, temp_cigar, col_rnext, col_pnext, col_tlen, temp_seq, col_qual[:seq_pointer], alignment_score)]+\
+                        Read.split_alignment_factory(col_qname, col_flag, col_rname, str(int(col_pos)+real_length), col_mapq, col_cigar[i+1:], col_rnext, col_pnext, col_tlen, col_seq[seq_pointer:], col_qual[seq_pointer:], alignment_score)
+                
+                # It is real hard to balance between the real next read starting position, the SEQ for current read and to where has the 
+                # CIGAR been processed.
+                if char == 'M' or char == 'X' or char == '=' or char =='I' or char=='S':
+                    temp_seq += col_seq[seq_pointer:seq_pointer+current_cigar_number]
+                    seq_pointer += current_cigar_number
+                    if char != 'I' and char != 'S':
+                        real_length += current_cigar_number
+                elif char == 'D':
+                    real_length += current_cigar_number
+                
+                temp_cigar += str(current_cigar_number) + char
+                # if it get to the end of the CIGAR string, then finish it.
+                if i == len(col_cigar)-1:
+                    return [Read(col_qname, col_flag, col_rname, col_pos, col_mapq, temp_cigar, col_rnext, col_pnext, col_tlen,\
+                                  temp_seq, col_qual[:len(temp_seq)], alignment_score)]
+                current_cigar_number = 0
+
+
+def load_vcf(opt):
+    '''
+    To load and do simple filtering on vcf file.
+    '''
+    # First try to figure out which column the sample corresponds to in vcf file.
+    # By literally get all the column names.
+    gzip_stream = gzip.open(opt.vcf_path, 'rt')
+    vcf_column_index_map = {}
+    for line in gzip_stream:
+        if line.strip().startswith('#CHR'):
+            column_names = line.strip().split('\t')
+            vcf_column_index_map = dict(map(lambda x: (x, column_names.index(x)), column_names))
+            break
+    gzip_stream.close()
+
+    if opt.sample_name not in vcf_column_index_map:
+        raise KeyError("sample_name {} not in vcf_column_index_map.keys(): {}".format(opt.sample_name, vcf_column_index_map.keys()))
+
+
+    # To restrict read-backed phasing to a given chr.
+    command_line = ''
+    if opt.restrict_chr is not None:
+        command_line = 'tabix -h ' + opt.vcf_path + ' ' + opt.restrict_chr + ':'
+    else:
+        command_line = 'gunzip -c ' + opt.vcf_path
+    
+    vcf_out = tempfile.NamedTemporaryFile(delete=False)
+    vcf_out.close()
+    processed_vcf_path = vcf_out.name
+
+
+    # Mean to add black-list to prevent unnecessary calculating.
+    # However I don't want to implement this
+    if opt.black_list is not None:
+        pass
+    else:
+        command_line += ' | cut -f 1-9,' + str(vcf_column_index_map[opt.sample_name]+1) + " | grep -v '0|0\|1|1' > " + processed_vcf_path
+        err_code = subprocess.check_call("set -euo pipefail && "+command_line, shell=True, executable='/bin/bash')
+
+    if err_code is None:
+        raise RuntimeError("Bash returned a err {} when executing {}".format(err_code, command_line))
+    
+    return processed_vcf_path
+
+
+def generate_variants(processed_vcf_path):
+    '''
+    To generate wrapped variants from processed VCF file.
+    Notablly, a typical VCF File header looks like:
+    #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	NA06986
+    '''
+    total_record_num = 0
+    filtered_record_num = 0
+
+    variants = []
+    with open(processed_vcf_path, 'r') as vcf_file:
+        for line in vcf_file:
+            if line.strip('\n').startswith('#'):
+                continue
+
+            total_record_num+=1
+            columns = line.strip('\n').split('\t')
+            col_chr, col_pos, col_id, col_ref, col_alt, col_qual, col_filter, col_info, col_format, col_sample = columns
+
+            # Simple filtering by $FILTER$ == PASS
+            if 'PASS' not in col_filter:
+                filtered_record_num+=1
+                continue
+
+            # Try to get annotated genotype in VCF
+            is_phased = False
+            format = col_format.split(':')
+            GT_index = None
+            if 'GT' in format:
+                GT_index = format.index('GT')
+            else:
+                filtered_record_num+=1
+                continue
+            
+            # Try to get Genotype string in VCF, w.r.t. 1/0 or 0/1 or 1|0 or 0|1
+            genotype_string = col_sample.split(':')[GT_index]
+            if '.' in genotype_string:
+                filtered_record_num += 1
+                continue
+            
+            # Restrict the length of alternative base pair into 1.
+            all_alleles = col_alt.split(',') + [col_ref]
+            if max([len(x) for x in all_alleles]) == 1:
+                variants.append(Variant(col_chr, col_pos, col_id, col_ref, col_alt, col_qual, genotype_string, is_phased))
+            else:
+                filtered_record_num += 1
+                continue
+    print('Received {} variants in total, {} variants taken, {} variants omitted.'.\
+          format(total_record_num, total_record_num-filtered_record_num, filtered_record_num))
+    
+    os.remove(processed_vcf_path)
+    
+    vid_var_map = {}
+    for var in variants:
+        vid_var_map[var.unique_id] = var
+
+    return variants, vid_var_map
+           
+
+def load_bam(opt, bed_file):
+    '''
+    To load and do simple filtering on BAM files.
+    '''
+    bam_out = tempfile.NamedTemporaryFile(delete=False)
+    bam_out.close()
+    output_sam_path = bam_out.name
+
+    # We don't need headers...for now.
+    command = "samtools view {} '{}' -F 0x400 -f 2 -q {} -L {} > {}".format(opt.bam_path, opt.restrict_chr, opt.mapq_threshold,\
+                                                                             bed_file, output_sam_path)
+    err_code = subprocess.check_call("set -euo pipefail && "+command, shell=True, executable='/bin/bash')
+
+    os.remove(bed_file)
+
+    return output_sam_path
+
+
+def generate_reads(opt, output_sam_path):
+    '''
+    Generate reads from filtered SAM file from give BAM file.
+    Notablly, reads is wrapped as a class with all fundamental informations in SAM file,
+    together with operations for calculating spans of the read.
+    '''
+    umibarcode_line_map = collections.defaultdict(list)
+    sam_file = pd.read_csv(output_sam_path, sep='\t', index_col=None, header=None)
+    sam_file = sam_file.to_numpy().tolist()
+    alignment_scores = []
+    for columns in sam_file:
+        col_qname, col_flag, col_rname, col_pos, col_mapq, col_cigar, col_rnext, col_pnext, col_tlen, col_seq, col_qual = \
+            columns[:11]
+        other_columns = columns[11:]
+        alignment_score = -1
+        col_rx, col_qx, col_bx, col_bc, col_qt = None, None, None, None, None, None
+        for col in other_columns:
+            if col.startswith('AS'):
+                alignment_score = int(col.split(':')[-1])
+            elif col.startswith('RX'):
+                col_rx = col.split(':')[-1]
+            elif col.startswith('QX'):
+                col_qx = col.split(':')[-1]
+            elif col.startswith('BX'):
+                col_bx = col.split(':')[-1]
+            elif col.startswith('BC'):
+                col_bc = col.split(':')[-1]
+            elif col.startswith('QT'):
+                col_qt = col.split(':')[-1]            
+
+        line = Bamline(col_qname, col_flag, col_rname, col_pos, col_mapq, col_cigar, col_rnext, col_pnext, col_tlen, col_seq,\
+                     col_qual, alignment_score, col_rx, col_qx, col_bx, col_bc, col_qt)
+        umibarcode_line_map['.'.join(col_bx, col_bc)].append(line)
+        alignment_scores.append(int(alignment_score))
+    os.remove(output_sam_path)
+
+    # Filter these reads by alignment score
+    alignment_score_filter = np.percentile(alignment_scores, opt.as_quality*100)
+
+    reads = []
+    for umi_barcode, lines in umibarcode_line_map.items():
+        reads.append(Read())
+
+    return reads
+
+def generate_bed_file(variants:list[Variant]):
+    
+    bed_file = tempfile.NamedTemporaryFile(delete=False, mode='wt')
+    for var in variants:
+        bed_file.write('{}\t{}\t{}\n'.format(var.col_chr, var.start, var.end))
+    bed_file.close()
+    return bed_file.name
+
+
+if __name__ == '__main__':
+    import argparse
+    opt = argparse.Namespace()
+    opt.restrict_chr = 'chr1'
+    opt.vcf_path = '../data/NA12878_phased_variants.vcf.gz'
+    opt.sample_name = 'NA12878'
+    opt.black_list = None
+    opt.bam_path = '../data/NA12878_phased_possorted_bam.bam'
+    opt.mapq_threshold = 255
+
+    processed_vcf = load_vcf(opt)
+    variants, vid_var_map = generate_variants(processed_vcf)
+    bed_file = generate_bed_file(variants)
+    output_sam_path = load_bam(opt, bed_file)
+    generate_reads(opt, output_sam_path)
