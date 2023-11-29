282992514a66870295c0653a6799484461939914
Laurentiusia <laurentiusia@LaurentiusiadeMacBook-Pro.local>
2023-11-29 13:26:42 +0800

    modified graph building strategy

--- utils/algo_utils.py
+++ utils/algo_utils.py
@@ -59,7 +59,6 @@ def extract_allele_linkage(read_variants_map: dict):
     allele_read_matchs = 0
     false_read_matchs = 0
     vars_ = set()
-    qnames = []
     # TO deal with paired reads. Map alleles to qnames instead of reads.
     qname_alleles_map = collections.defaultdict(list)
     for read, variants in read_variants_map.items():
@@ -74,7 +73,7 @@ def extract_allele_linkage(read_variants_map: dict):
             times = len(allele)
             allele_read_matchs += 1
             geno = var.get_geno_by_allele(allele[0])
-            qname_alleles_map[read.umi_barcode]+=[var.unique_id+':'+str(geno)]*times
+            qname_alleles_map[read.umi_barcode]+=(var.unique_id+':'+str(geno), times)
     # there's two ways of implementation
     # first is just link the closest pair of alleles on reads
     # second is link a allele with all the alleles on a same read.
@@ -83,12 +82,11 @@ def extract_allele_linkage(read_variants_map: dict):
     # but the second will not discard any read information
     allele_linkage_map = collections.defaultdict(int)
     for qname, allele_list in qname_alleles_map.items():
-        allele_list = sorted(list(set(allele_list)))
         for i in range(0, len(allele_list)-1):
             for j in range(i+1, len(allele_list)):
-                allele_linkage_map[(allele_list[i], allele_list[j])] += 1
-                vars_.add(allele_list[i].split(':')[0])
-                vars_.add(allele_list[j].split(':')[0])
+                allele_linkage_map[(allele_list[i][0], allele_list[j][0])] += min(allele_list[i][0], allele_list[j][0])
+                vars_.add(allele_list[i][0].split(':')[0])
+                vars_.add(allele_list[j][0].split(':')[0])
     print('There are {} pseudo matches, {} variants has at least 1 neighbors, among which {} are considered matched and {} are false matches'\
           .format(allele_read_matchs+false_read_matchs, len(vars_), allele_read_matchs, false_read_matchs))
     return allele_linkage_map
